e2da41c951dbdba96a09e8584ee9e9aa515787d5
Василий Ямутин <iamutin@yandex.ru>
2024-02-18 20:43:50 +0300

    Complete the project

---
+++ blogicum/blog/mixins.py
@@ -0,0 +1,37 @@
+from blog.forms import PostForm
+from blog.models import Comment, Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+
+
+class CommentUpdateDeleteMixin(LoginRequiredMixin):
+    model = Comment
+    pk_url_kwarg = 'comment_id'
+    template_name = 'blog/comment.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        instance = get_object_or_404(Comment, pk=kwargs[self.pk_url_kwarg])
+        if instance.author != request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse(
+            'blog:post_detail',
+            kwargs={'post_id': self.kwargs.get('post_id')}
+        )
+
+
+class PostUpdateDeleteMixin(LoginRequiredMixin):
+    model = Post
+    form_class = PostForm
+    pk_url_kwarg = 'post_id'
+    template_name = 'blog/create.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        instance = get_object_or_404(Post, pk=kwargs[self.pk_url_kwarg])
+        if instance.author != request.user:
+            return redirect('blog:post_detail', kwargs[self.pk_url_kwarg])
+        return super().dispatch(request, *args, **kwargs)
